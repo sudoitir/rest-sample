8132c654e333d4588a399bba81038edc404b607f
Mahdi Amirabdolahi <sudoit.ir@gmail.com>
2024-03-07 12:33:38 +0330

    Improvement

--- src/main/java/ir/sudoit/restsample/RestSampleApplication.java
+++ src/main/java/ir/sudoit/restsample/RestSampleApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableFeignClients
-@EnableWebMvc
 @SpringBootApplication
 public class RestSampleApplication {
 
